efd63c7ab25084ac32e9758626bef890657ce5ea
Pegdwende12 <pegdwendeilboudo564@gmail.com>
2023-07-03 03:23:16 +0000

    ajout des fichiers.blade et amelioration

---
+++ resources/views/Acceuil.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>My forum</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Titillium+Web:ital,wght@0,400;0,700;1,400&display=swap')}}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('css/style1.css')}}">
+</head>
+<body>
+        <div class="container-fluid">
+        <!-- Background animtion-->
+            <div class="background">
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+        </div>
+
+
+    <header>
+        <div class="navbar">
+            <nav >
+                <ul >
+                    
+                    <li class="nav-item">
+                        <a href="#">Acceuil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('espacepublic')}}">Forum</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Inscription</a>
+                    </li>
+                </ul>
+            </nav>
+            
+
+        </div>
+
+            <section class="header-content">
+                <h1>Bienvenue</h1>
+                <p> Bienvenue sur le forum de discussion. Partagez vos connaissances
+                     et tirez parti des connaissances de vos camarades</p>
+                <button>
+                    <a href="{{route('espacepublic')}}">Rejoindre la discussion</a>
+                </button>
+                
+            </section>
+       
+    </header>
+    
+
+    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/espacepublicLien.blade.php
@@ -0,0 +1,117 @@
+@extends('master')
+
+@section('meta')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@endsection   
+
+@section('title')
+    <title>My forum</title>
+
+@section('content')
+    
+
+    <div class="container">
+        <div class="subforum">
+            <div class="navigate">
+                <span><a href="#">My Forum - images</a> >>
+                    <a href="#">Videos</a>>> 
+                    <a href="#">Resume text</a> >> 
+                    <a href="#">liens </a> 
+                    <button>
+                       <a href="#">Poster une connaissance</a>  
+                    </button>
+                </span>
+            </div>
+
+            <div class="subforum-title">
+                <h1>Nom de la matiere</h1>
+            </div>
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="far fa-thumbs-up"></i>
+                    <br>
+                    <i class="far fa-thumbs-down"></i>
+                </div>
+                <div class="subforum-description subforum-column ">
+                    <h1><a href="">titre de la connaissance: </a></h1>
+                    <p> 
+                        <a href="">Lien vers la connaissance</a>
+                    </p>
+                    
+                </div>
+                <div class="subforum-stats subforum-column center">
+                    <span id="view-count">12 </span>  vues
+                    <span id="like-count"> 14 </span> likes
+                </div>
+                <div class="subforum-info subforum-column ">
+                    <b><a href=""> Posté </a></b> par <a href="">Nom de l'etudiant</a>
+                    <br>
+                    le<small>date  et l'heure a laquelle il a posté</small>
+                </div>
+            </div>
+
+
+
+            <div class="subforum-title">
+                <h1>Nom de la matiere</h1>
+            </div>
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="far fa-thumbs-up"></i>
+                    <br>
+                    <i class="far fa-thumbs-down"></i>
+                </div>
+                <div class="subforum-description subforum-column ">
+                    <h1><a href="">titre de la connaissance: </a></h1>
+                    <p> 
+                        <a href="">Lien vers la connaissance</a>
+                    </p>
+                    
+                </div>
+                <div class="subforum-stats subforum-column center">
+                    <span id="view-count">12 </span>  vues
+                    <span id="like-count"> 14 </span> likes
+                </div>
+                <div class="subforum-info subforum-column ">
+                    <b><a href=""> Posté </a></b> par <a href="">Nom de l'etudiant</a>
+                    <br>
+                    le<small>date  et l'heure a laquelle il a posté</small>
+                </div>
+            </div>
+
+
+            <div class="subforum-title">
+                <h1>Nom de la matiere</h1>
+            </div>
+            <div class="subforum-row">
+                <div class="subforum-icon subforum-column center">
+                    <i class="far fa-thumbs-up"></i>
+                    <br>
+                    <i class="far fa-thumbs-down"></i>
+                </div>
+                <div class="subforum-description subforum-column ">
+                    <h1><a href="">titre de la connaissance: </a></h1>
+                    <p> 
+                        <a href="">Lien vers la connaissance</a>
+                    </p>
+                    
+                </div>
+                <div class="subforum-stats subforum-column center">
+                    <span id="view-count">12 </span>  vues
+                    <span id="like-count"> 14 </span> likes
+                </div>
+                <div class="subforum-info subforum-column ">
+                    <b><a href=""> Posté </a></b> par <a href="">Nom de l'etudiant</a>
+                    <br>
+                    le<small>date  et l'heure a laquelle il a posté</small>
+                </div>
+            </div>
+
+
+            
+        </div>
+
+
+    </div>
+@endsection
